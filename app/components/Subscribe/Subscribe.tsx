c22d4b6dbf042e18dcb9c85f9bacb20154321b94
akky <developerakky@gmail.com>
2024-07-17 08:11:44 +0530

    subscribe sec done

--- app/components/Subscribe/Subscribe.tsx
+++ app/components/Subscribe/Subscribe.tsx
@@ -3,7 +3,28 @@ import React from "react";
 const Subscribe = () => {
   return (
     <>
-      <div className="pt-[5rem] pb-[5rem]"></div>
+      <div className="pt-[5rem] pb-[5rem]">
+        <div className="text-center">
+          <h1 className="text-[25px] sm:text-[38px] md:text-[45px] text-black font-bold uppercase">
+            JOIN FOR HOT <span className="text-red-600">OFFERS</span>
+          </h1>
+          <p className="text-black text-opacity-70 w-[80%] mx-auto ">
+            Hey Users!! Subscribe to our store and get crazy offer on every
+            meal...
+          </p>
+
+          <div className="flex items-center space-x-2 mt-[2rem] justify-center">
+            <input
+              type="mail"
+              placeholder="Enter Email"
+              className="px-4 py-3 lg:px-6 lg:py-4 bg-gray-300 w-[50%] outline-none rounded-lg placeholder:text-black"
+            ></input>
+            <button className="px-5 py-3 lg:px-8 lg:py-4 bg-green-700 hover:bg-green-900 rounded-md text-white transition-all delay-100 ">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
